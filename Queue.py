7c61a2821126bc80dc9b69f924392e879fecad82
Jonathan Ebel <jonathan@tigabyte.de>
2025-11-05 22:02:18 +0100

    New ADT : Stack + Docs in README.md

---
+++ Queue.py
@@ -0,0 +1,9 @@
+# Phython Implementation of a Queue
+#
+#
+
+
+
+class Queue:
+    def __init__(self):
+        self.item
---
+++ Stack.py
@@ -0,0 +1,48 @@
+# Implementation of a Stack
+#
+#
+
+class Stack:
+
+    def __init__(self):
+        self.items = []
+
+    def push(self, item):
+        self.items.append(item)
+
+    def pop(self):
+        if not self.isEmpty():
+            return self.items.pop()
+        else:
+            raise IndexError("Stack is empty, there is not object left to pop")
+
+    def peek(self):
+        if not self.isEmpty():
+            return self.items[-1]
+        else:
+            raise IndexError("Stack is empty, there is not object left to peek")
+
+    def isEmpty(self):
+        return len(self.items) == 0
+
+    def size(self):
+        return len(self.items)
+
+
+
+def test():
+    stack = Stack()
+    stack.push(1)
+    stack.push(2)
+    stack.push(3)
+    stack.push(4)
+
+    print(f"popped: {stack.pop()}")
+    print(f"popped: {stack.pop()}")
+    print(f"size of stack: {stack.size()}")
+    print(f"popped: {stack.pop()}")
+    print(f"popped: {stack.pop()}")
+    print(f"Is Empty? : {stack.isEmpty()}")
+
+
+test()
