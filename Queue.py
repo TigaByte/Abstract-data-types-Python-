066a8c0450d3b8e6b40e65d2116f49d823380736
Jonathan Ebel <jonathan@tigabyte.de>
2025-11-05 22:12:18 +0100

    New ADT : parts of Queue

--- Queue.py
+++ Queue.py
@@ -3,7 +3,28 @@
 #
 
 
+class Queue():
 
-class Queue:
     def __init__(self):
-        self.item
+        self.items = []
+
+    def enqueue(self, item):
+        self.items.append(item)
+
+    def dequeue(self):
+        if not self.isEmpty():
+            return self.items.pop()
+        else:
+            raise IndexError("Queue is empty")
+
+    def front(self):
+        if not self.isEmpty():
+            return self.items[0]
+        else:
+            raise IndexError("Queue is empty")
+
+    def isEmpty(self):
+        return len(self.items) == 0
+
+    def size(self):
+        return len(self.items)
